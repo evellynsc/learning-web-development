f40d7169d1769a7e9518150142e1ecec442c655c
Evellyn <evellyn.cavalcante@gmail.com>
2016-11-04 16:18:35 -0200

    Add routing

--- angular2/tourofHeroes/app/app.module.ts
+++ angular2/tourofHeroes/app/app.module.ts
@@ -1,19 +1,32 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+
 
 //You compose Angular applications into closely related blocks of functionality with NgModules.
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+  ],
+  providers: [
+    HeroService,
   ],
   bootstrap:    [ AppComponent ]
 })
